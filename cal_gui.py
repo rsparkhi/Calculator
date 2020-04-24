f66922bb60a9dd383691118cf025364a6a4279cf
Ritesh Parkhi <52211413+rsparkhi@users.noreply.github.com>
2020-04-24 16:40:09 +0530

    Started

---
+++ cal_gui.py
@@ -0,0 +1,16 @@
+import re
+import kivy
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+
+class CalGridLayout(GridLayout):
+    pass
+
+class CalculatorApp(App):
+    def build(self):
+        return CalGridLayout()
+
+CalculatorApp().run()
+print("Ritesh")
